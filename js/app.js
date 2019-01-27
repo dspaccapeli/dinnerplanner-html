9538296af254b3ca6e676d75027193993674401f
Daniele Spaccapeli <>
2019-01-27 18:47:46 +0100

    gsc functions

--- js/app.js
+++ js/app.js
@@ -27,4 +27,85 @@ $(function() {
 	 * of the specific view you're working with (see exampleView.js).
 	 */
 
+	let showMenu = function() {
+        $("#menu").show();
+    };
+
+    let hideMenu = function() {
+        $("#menu").hide();
+    };
+
+    let showSelectDish = function() {
+        $("#dishes").show();
+    };
+
+    let hideSelectDish = function() {
+        $("#dishes").hide();
+    };
+
+    let showTopbar = function() {
+        $("#topbar").show();
+    };
+
+    let hideTopbar = function() {
+        $("#topbar").hide();
+    };
+
+    let showPrintoutView = function() {
+        $("#dinner_printout").show();
+    };
+
+    let hidePrintoutView = function() {
+        $("#dinner_printout").hide();
+    };
+
+    let showDishDetails = function() {
+        $("#content").show();
+    };
+
+    let hideDishDetails = function() {
+        $("#content").hide();
+    };
+
+    let showDinnerOverview = function() {
+        $("#content").show();
+    };
+
+    let hideDinnerOverview = function() {
+        $("#content").hide();
+    };
+
+    let hideAllViews = function() {
+        hideMenu();
+        hideSelectDish();
+        hideTopbar();
+        hidePrintoutView();
+        hideDishDetails();
+        hideDinnerOverview();
+    };
+
+    this.showSelect = function() {
+        hideAllViews();
+        showMenu();
+        showSelectDish();
+    };
+
+    this.showPrintout = function() {
+        hideAllViews();
+        showTopbar();
+        showPrintoutView();
+    };
+
+    this.showDetails = function() {
+        hideAllViews();
+        showMenu();
+        showDishDetails();
+    };
+
+    this.showOverview = function() {
+        hideAllViews();
+        showTopbar();
+        showDinnerOverview();
+    };
+
 });
